1f8bca61c8e5e7c3205f86bd2310550121da7542
va1kar1e <siwanont1@gmail.com>
2020-09-27 23:54:48 +0700

    Rename MAIN file

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ a = input('N')
 # Define the codec and create VideoWriter object
 # fourcc = cv2.VideoWriter_fourcc(*'MJPG')
 #out = cv2.VideoWriter('output.avi',fourcc, 20.0, (640,480))
-out = cv2.VideoWriter('output'+ a +'.avi', -1, 20.0, (640, 480))
+out = cv2.VideoWriter('output' + a + '.avi', -1, 20.0, (640, 480))
 
-while(cap.isOpened()):
+while (cap.isOpened()):
     ret, frame = cap.read()
     if ret == True:
         frame = cv2.flip(frame, 1)
